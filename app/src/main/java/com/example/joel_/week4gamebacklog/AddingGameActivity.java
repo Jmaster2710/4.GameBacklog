229dd218270c917292e7df63ae14628619532f18
Jmaster2710 <joel_kroon@hotmail.com>
2018-10-11 14:36:21 +0200

    Added the second screen properly

---
+++ app/src/main/java/com/example/joel_/week4gamebacklog/AddingGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.joel_.week4gamebacklog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddingGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_game);
+    }
+}

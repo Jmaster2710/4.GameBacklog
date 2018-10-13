35f75008ebb94f67911fa16bab8e11cb01cc8d62
Jmaster2710 <joel_kroon@hotmail.com>
2018-10-13 13:38:46 +0200

    Fixed the app from crashing

--- app/src/main/java/com/example/joel_/week4gamebacklog/MainActivity.java
+++ app/src/main/java/com/example/joel_/week4gamebacklog/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -48,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        final RecyclerView mRecyclerView = findViewById(R.id.RecyclerView);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new GameAdapter(this , listGames);
+        mRecyclerView.setAdapter(mAdapter);
+
+        updateUI();
     }
 
     @Override
